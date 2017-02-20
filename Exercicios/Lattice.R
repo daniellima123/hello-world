110f1278ff8bcad0a5db5577f68eeeb590aac28a
daniellima123 <daniel.limaviegas454@gmail.com>
2017-02-20 20:51:57 -0300

    Exercicio

---
+++ Exercicios/Lattice.R
@@ -0,0 +1,23 @@
+#Exercício 1
+#Easy Mode. Usando o banco de dados da Pasta DataSets construa um gráfico de
+#barras com as funções básicas e com o Lattice.
+
+
+
+#Exercício 2
+#Not so easy. Com o pacote básico do R construa um gráfico de setores da variá-
+#vel region e um histograma da variável Land.Value. No caso do gráfico de seto-
+#res escolha quais cores irão aparecer.
+
+
+
+#Exercício 3
+#Hard way. Com o pacote Lattice Faça um gráfico de dispersão da variável Land.Value
+#explicada, porém os dados devem conter apenas os anos posteriores a 2005 e o Land.Value
+#deve ser maior que 50.000(cinquenta mil).
+#PS.: Não pode armazenar um subset em outro objeto.
+
+
+
+
+#Bons estudos.
